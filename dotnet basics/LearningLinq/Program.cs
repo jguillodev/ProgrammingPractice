6b972bd090ea8f8270ab488bac5ab28aba1c3b18
jguillodev <joshua.guillory77@gmail.com>
2020-09-27 15:17:11 -0400

    Programming Linq Statements

---
+++ dotnet basics/LearningLinq/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace LearningLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Bil","Steve","James","Mohan" };
+
+            var myLinqQuery = from name in names where name.Contains('a') select name;
+
+            foreach (var name in myLinqQuery) 
+            {
+                Console.Write($"{name},  ");
+            }
+
+            Student[] studentArray = 
+                {
+                    new Student() { StudentID = 1, StudentName ="John", Age = 18 },
+                
+                };
+                
+        }
+    }
+
+    internal class Student 
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int Age { get; set; }
+    }
+}
