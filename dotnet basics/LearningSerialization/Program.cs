410cfcdaeb20a6a7dc97c8d7011fd4efbcea2252
jguillodev <joshua.guillory77@gmail.com>
2020-10-01 13:22:59 -0400

    Add Angular and Javascript

--- dotnet basics/LearningSerialization/Program.cs
+++ dotnet basics/LearningSerialization/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace LearningSerialization
 {
@@ -6,7 +7,53 @@ namespace LearningSerialization
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DirectoryInfo di = new DirectoryInfo(@"C:\Users\joshu\Desktop\DI");
+            string path = (@"C:\Users\joshu\Desktop\DI\di.txt");
+
+            if (!File.Exists(path))
+            {
+                //Create a file to write to 
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine("Hello");
+                    sw.WriteLine("And");
+                    sw.WriteLine("Welcome");
+                }
+
+              
+            }
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                string s;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+
+
+            try
+            {
+                if (di.Exists)
+                {
+                    Console.WriteLine("That path exist already.");
+                    return;
+                }
+                else
+                {
+                    //Try to create the directory
+                    di.Create();
+                    Console.WriteLine("The directory was created successfully");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The process failed: {0}", e.ToString());
+            }
+            finally { }
+
+          
         }
     }
 }
