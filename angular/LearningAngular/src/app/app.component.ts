679fff23821872c6116ce3f29f071bad546b1f4d
jguillodev <joshua.guillory77@gmail.com>
2020-10-01 23:51:43 -0400

    Updated Angular Component Life Cycle

--- angular/LearningAngular/src/app/app.component.ts
+++ angular/LearningAngular/src/app/app.component.ts
@@ -3,8 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  //template: '<h1>Component is the main Building Block in Angluar</h1> <h2>Angular 8 Samples</h2>',
+  //styles: ['h1{color:red; font-weight:bold;}','h2{color:blue}']
+  styleUrls:['custom.css']
+  
 })
 export class AppComponent {
   title = 'LearningAngular';
+
+  data:number =100;
+  constructor()
+  {
+    console.log(`new - data is${this.data}` );
+  }
+
+  ngOnChanges()
+  {
+    console.log(`ngOnChanges - data is ${this.data}`);
+  }
+  ngOnInit()
+  {
+    console.log(`ngOnInit - data is ${this.data}`);
+  }
 }
